8814663518854396c73e971f18de9d3f9d50e133
Alifouaad24 <161458284+Alifouaad24@users.noreply.github.com>
2024-09-17 16:31:19 -0700

    DriverInfoApi

--- Infarstuructre/BL/CLSTBDriverInformation.cs
+++ Infarstuructre/BL/CLSTBDriverInformation.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using Microsoft.EntityFrameworkCore;
+
 namespace Infarstuructre.BL
 {
     public interface IIDriverInformation
@@ -10,7 +12,13 @@ namespace Infarstuructre.BL
         bool UpdateData(TBDriverInformation updatss);
         bool deleteData(int IdDriverInformation);
         List<TBViewDriverInformation> GetAllv(int IdDriverInformation);
-
+        // ////////////////////////API/////////////////////////////////////
+        Task<List<TBViewDriverInformation>> GetAllAsync();
+        Task<TBDriverInformation> GetByIdAsync(int IdDriverInformation);
+        Task<bool> AddDataAsync(TBDriverInformation savee);
+        Task<bool> UpdateDataAsync(TBDriverInformation updatss);
+        Task<bool> DeleteDataAsync(int IdDriverInformation);
+        Task<List<TBViewDriverInformation>> GetAllvAsync(int IdDriverInformation);
     }
 
     public class CLSTBDriverInformation: IIDriverInformation
@@ -77,6 +85,72 @@ namespace Infarstuructre.BL
         public List<TBViewDriverInformation> GetAllv(int IdDriverInformation)
         {
             List<TBViewDriverInformation> MySlider = dbcontext.ViewDriverInformation.OrderByDescending(n => n.IdDriverInformation == IdDriverInformation).Where(a => a.IdDriverInformation == IdDriverInformation).Where(a => a.CurrentState == true).ToList();
+            return MySlider;
+        }
+
+        // /////////////////////////////////////////API//////////////////////////////////
+
+        public async Task<List<TBViewDriverInformation>> GetAllAsync()
+        {
+            List<TBViewDriverInformation> MySlider = await dbcontext.ViewDriverInformation.OrderByDescending(n => n.IdDriverInformation).Where(a => a.CurrentState == true).ToListAsync();
+            return MySlider;
+        }
+
+        public async Task<TBDriverInformation> GetByIdAsync(int IdDriverInformation)
+        {
+            TBDriverInformation sslid = await dbcontext.TBDriverInformations.FirstOrDefaultAsync(a => a.IdDriverInformation == IdDriverInformation);
+            return sslid;
+        }
+
+        public async Task<bool> AddDataAsync(TBDriverInformation savee)
+        {
+            try
+            {
+                await dbcontext.AddAsync<TBDriverInformation>(savee);
+                await dbcontext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateDataAsync(TBDriverInformation updatss)
+        {
+            try
+            {
+                dbcontext.Entry(updatss).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                await dbcontext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteDataAsync(int IdDriverInformation)
+        {
+            try
+            {
+                var catr = await GetByIdAsync(IdDriverInformation);
+                catr.CurrentState = false;
+                //TbSubCateegoory dele = dbcontex.TbSubCateegoorys.Where(a => a.IdBrand == IdBrand).FirstOrDefault();
+                //dbcontex.TbSubCateegoorys.Remove(dele);
+                dbcontext.Entry(catr).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                await dbcontext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<TBViewDriverInformation>> GetAllvAsync(int IdDriverInformation)
+        {
+            List<TBViewDriverInformation> MySlider = await dbcontext.ViewDriverInformation.OrderByDescending(n => n.IdDriverInformation == IdDriverInformation).Where(a => a.IdDriverInformation == IdDriverInformation).Where(a => a.CurrentState == true).ToListAsync();
             return MySlider;
         }
     }
---
+++ Yara/Areas/Admin/APIsControllers/DriverInformationAPIController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Yara.Areas.Admin.APIsControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverInformationAPIController : ControllerBase
+    {
+        IIDriverInformation iDriverInfo;
+        ApiResponse response;
+        public DriverInformationAPIController(IIDriverInformation iDriverInfo1)
+        {
+            iDriverInfo = iDriverInfo1;
+            response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var allData = await iDriverInfo.GetAllAsync();
+                if (allData == null)
+                    response.StatusCode = System.Net.HttpStatusCode.NotFound;
+
+                response.Result = allData;
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = new List<string> { ex.Message };
+                response.IsSuccess = false;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpGet("GetById")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var allData = await iDriverInfo.GetByIdAsync(id);
+                if (allData == null)
+                    response.StatusCode = System.Net.HttpStatusCode.NotFound;
+
+                response.Result = allData;
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = new List<string> { ex.Message };
+                response.IsSuccess = false;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddData([FromBody] TBDriverInformation data)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+
+                await iDriverInfo.AddDataAsync(data);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = new List<string> { ex.Message };
+                response.IsSuccess = false;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateData([FromBody] TBDriverInformation data)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+
+                await iDriverInfo.UpdateDataAsync(data);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = new List<string> { ex.Message };
+                response.IsSuccess = false;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteData(int id)
+        {
+            try
+            {
+                var item = await GetById(id);
+                if (item == null)
+                    response.StatusCode = System.Net.HttpStatusCode.NotFound;
+
+                await iDriverInfo.DeleteDataAsync(id);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = new List<string> { ex.Message };
+                response.IsSuccess = false;
+            }
+
+            return Ok(response);
+        }
+    }
+}
